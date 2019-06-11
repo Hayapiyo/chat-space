824de97156b08e220846b3fa18f0ba909f855296
hayaYoshi18 <yoshiyuki.hayashi@di-v.co.jp>
2019-06-11 15:15:56 +0900

    add_message_in_ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,16 @@
 $(document).on('turbolinks:load', function(){
+  function buildHTML(message) {
+    var img = message.image ? "${message.image}" : ""
+    var html = `<div class="message__upper-info">
+                  <p class="message__upper-info__talker">${ message.user_name }</p>
+                  <p class="message__upper-info__date">${ message.created_at }</p>
+                </<div>
+                <div class="message__text">
+                  ${ message.content ? message.content : "" }
+                  <img src=${ img }>
+                </div>`
+              return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var message = new FormData(this);
@@ -10,6 +22,15 @@ $(document).on('turbolinks:load', function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message').append(html);
+      $('#message_content').val('');
+
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信されませんでした。')
     })
   })
 });
