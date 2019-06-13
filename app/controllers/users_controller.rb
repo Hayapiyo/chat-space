e5d2d9c3c1a35f8a4fea26c3eb2399e485c8dbfd
hayaYoshi18 <yoshiyuki.hayashi@di-v.co.jp>
2019-06-13 09:31:13 +0900

    add_incremental-search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")  # 基礎カリキュラムでここを復習
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
@@ -11,6 +16,9 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def search
   end
 
   private
